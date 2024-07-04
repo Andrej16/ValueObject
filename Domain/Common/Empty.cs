dcd5a3aa5e4135826abc6a883b4fc60bdd19c4de
Andrii Shylin <31798010+Andrej16@users.noreply.github.com>
2024-07-04 16:40:26 +0300

    Added empty class for cases with void return value

---
+++ Domain/Common/Empty.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Common;
+
+public sealed class Empty : IEquatable<Empty>, IComparable<Empty>, IComparable
+{
+    public static Empty Value { get; } = new();
+
+    public static Task<Empty> Task { get; } = System.Threading.Tasks.Task.FromResult(Value);
+
+    public bool Equals(Empty? other) => other is not null;
+
+    public override bool Equals(object? obj) => obj is Empty;
+
+    public int CompareTo(Empty? other) => other is not null ? 0 : throw new ArgumentNullException();
+
+    public int CompareTo(object? obj) => obj is Empty ? 0 : throw new ArgumentException();
+
+    public override int GetHashCode() => 0;
+
+    public static bool operator ==(Empty left, Empty right) => left.Equals(right);
+
+    public static bool operator !=(Empty left, Empty right) => !left.Equals(right);
+
+    public override string ToString() => "()";
+}
+
